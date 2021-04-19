36b2272b9bd58d43bf6c1270d2d7e6c6f9f71ec4
dragsec <draganyes@gmail.com>
2021-04-19 14:39:29 +0200

    beta

--- Pari e Dispari/script.js
+++ Pari e Dispari/script.js
@@ -1,5 +1,24 @@
+var scelta = prompt("Pari o dispari?")
+console.log("Hai scelto " + scelta)
 var numeroUtente = prompt("Inserisci un numero");
 console.log("Il tuo numero è " + numeroUtente);
 var numeroComputer = Math.floor(Math.random()*5);
 console.log("Il numero del computer è " + numeroComputer);
+
+function calcolo(){
+    var somma = numeroUtente + numeroComputer;
+    if (somma % 2 === 0){ 
+    return "pari" 
+    }else{
+    return "dispari" 
+    }
+}
+
+var risultato = calcolo();
+
+if(scelta.toLowerCase() == risultato.toLowerCase()){
+    console.log("Hai vinto!")
+}else{
+    console.log("Hai perso!")
+}
 
