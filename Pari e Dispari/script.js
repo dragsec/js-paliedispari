1c10ed91b03a5b30d18d94a2e7b23d0de0aab6f2
dragsec <draganyes@gmail.com>
2021-04-19 14:28:00 +0200

    inizio

---
+++ Pari e Dispari/script.js
@@ -0,0 +1,5 @@
+var numeroUtente = prompt("Inserisci un numero");
+console.log("Il tuo numero è " + numeroUtente);
+var numeroComputer = Math.floor(Math.random()*5);
+console.log("Il numero del computer è " + numeroComputer);
+
