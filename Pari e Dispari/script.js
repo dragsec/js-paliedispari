12c3da898325163be407b8a48be54de41953edce
dragsec <draganyes@gmail.com>
2021-04-19 14:47:19 +0200

    funziona

--- Pari e Dispari/script.js
+++ Pari e Dispari/script.js
@@ -2,11 +2,12 @@ var scelta = prompt("Pari o dispari?")
 console.log("Hai scelto " + scelta)
 var numeroUtente = prompt("Inserisci un numero");
 console.log("Il tuo numero è " + numeroUtente);
-var numeroComputer = Math.floor(Math.random()*5);
+var numeroComputer = Math.ceil(Math.random()*5);
 console.log("Il numero del computer è " + numeroComputer);
+var somma = (1*numeroUtente) + (1*numeroComputer);
+console.log("La somma è " + somma)
 
 function calcolo(){
-    var somma = numeroUtente + numeroComputer;
     if (somma % 2 === 0){ 
     return "pari" 
     }else{
